7f60dbc700c1432bd48c4e2e8d55957a1cae55a6
Matt Bloomfield <mabl4638@colorado.edu>
2023-11-09 13:24:48 -0700

    finished wa11

--- gallery-start/main.js
+++ gallery-start/main.js
@@ -5,14 +5,39 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const picarray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 /* Declaring the alternative text for each image file */
-
+const picarrayAlt = {
+    'pic1.jpg': 'A beautiful landscape',
+    'pic2.jpg': 'A beautiful landscape',
+    'pic3.jpg': 'A beautiful landscape',
+    'pic4.jpg': 'A beautiful landscape',
+    'pic5.jpg': 'A beautiful landscape'
+};
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for(const image of picarray) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', picarrayAlt[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e => {
+        displayedImage.src = e.target.src;
+        displayedImage.alt = e.target.alt;
+    });
+}
 
 /* Wiring up the Darken/Lighten button */
+
+
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if(btnClass === 'dark') {
+        btn.setAttribute('class','light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+        btn.setAttribute('class','dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+});
