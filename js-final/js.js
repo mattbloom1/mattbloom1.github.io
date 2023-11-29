395204c7e4d7f3a6ff4afd090c92af164cb8e3b7
Matt Bloomfield <mabl4638@colorado.edu>
2023-11-29 11:01:14 -0700

    startofJSfinal

---
+++ js-final/js.js
@@ -0,0 +1,68 @@
+const btn = document.getElementById("spin");
+btn.addEventListener("click", roll);
+
+const btnAdd = document.getElementById("addNum");
+btnAdd.addEventListener("click", conPhoneNum);
+
+const btnClear = document.getElementById("clearNum");
+btnClear.addEventListener("click", clearPhoneNumber);
+
+const result = document.getElementById("result");
+const winNum = document.getElementById("winNum");
+
+
+var allNumbers = [];
+var number = [];
+var count = 0;
+var winNumb = 0;
+
+function roll() {
+    if(number.length < 10){
+        var randomNum = Number((Math.random() * 100).toFixed(2));
+        var firstDigit = Number(Math.floor((randomNum) / 10 - 5));
+        var win = firstDigit;
+        if (win > 4) {
+            var rollm = win * 40 - 40 * 15;
+            document.getElementById("roll").style.marginTop = rollm + "px";
+        }
+        if (win < 4) {
+            var rollm = 180 - 40 * win - 380;
+            document.getElementById("roll").style.marginTop = rollm + "px";
+        }
+        if (win == 4) {
+            var rollm = 360;
+            document.getElementById("roll").style.marginTop = "-" + rollm + "px";
+        }
+        allNumbers.push(Math.floor(randomNum/10));
+        winNumb = Math.floor(randomNum/10);
+        count++;
+        displayWinningNumber();
+
+    }
+    else{
+        alert("You have reached the maximum number of spins.");
+    }
+}
+
+function conPhoneNum(){
+    if(number.length < 10){
+    number.push(allNumbers[count-1]);
+    }
+    else{
+        alert("You have reached the maximum number of digits.");
+    }
+    displayPhoneNumber();
+}
+function displayWinningNumber() {
+    var winningNumber = winNumb;
+    winNum.innerText = "Number to be added - " + winningNumber;
+}
+function clearPhoneNumber() {
+    number = [];
+    displayPhoneNumber();
+}
+function displayPhoneNumber() {
+    var phoneNumber = number.join("");
+    var formattedPhoneNumber = phoneNumber.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
+    result.innerText = formattedPhoneNumber;
+}
